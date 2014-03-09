1a6475ce1829c22c722f7be07d5233b4b79934f4
shaojianhong <shaojianhong@shao.mshome.net>
2014-03-09 23:37:42 +0800

    提交

--- CCC_Test/src/test/com/ccc/dreamfile/Test_FilePropertiesUtil.java
+++ CCC_Test/src/test/com/ccc/dreamfile/Test_FilePropertiesUtil.java
@@ -1,11 +1,24 @@
 package test.com.ccc.dreamfile;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+import com.ccc.dreamfile.properties.FilePropertiesUtil;
+
 /**
- *
  * @author RedSword
  * @date 2014年3月8日 下午11:05:55
  */
 
 public class Test_FilePropertiesUtil {
-
+	public static void main(String[] args) {
+		// 读取properties
+		HashMap<String, String> map = FilePropertiesUtil.readProperties(new File("E:/1.properties"));
+		for (Entry<String, String> en : map.entrySet()) {
+			System.out.println(en.getKey() + "-" + en.getValue());
+		}
+		// 创建properties文件
+		FilePropertiesUtil.createProperties(map, new File("E:/2.properties"), false);
+	}
 }
