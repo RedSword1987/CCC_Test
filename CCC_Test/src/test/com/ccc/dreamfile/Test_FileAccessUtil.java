bc692a14b0d85ef14bb822ea3565ed2defaee08c
shaojianhong <shaojianhong@shao.mshome.net>
2014-03-09 22:36:35 +0800

    提交

--- CCC_Test/src/test/com/ccc/dreamfile/Test_FileAccessUtil.java
+++ CCC_Test/src/test/com/ccc/dreamfile/Test_FileAccessUtil.java
@@ -1,11 +1,12 @@
 package test.com.ccc.dreamfile;
 
 /**
- *
  * @author RedSword
  * @date 2014年3月8日 下午11:10:43
  */
 
 public class Test_FileAccessUtil {
-
+	public static void main(String[] args) {
+		// 此类用的不多，暂时不提供测试用例
+	}
 }
--- CCC_Test/src/test/com/ccc/dreamfile/Test_FileCsvUtil.java
+++ CCC_Test/src/test/com/ccc/dreamfile/Test_FileCsvUtil.java
@@ -1,11 +1,17 @@
 package test.com.ccc.dreamfile;
 
+import com.ccc.dreamfile.csv.FileCsvUtil;
+
 /**
- *
  * @author RedSword
  * @date 2014年3月8日 下午11:07:46
  */
 
 public class Test_FileCsvUtil {
-
+	public static void main(String[] args) {
+		// 将csv文件转换为 List<List<String>> 格式
+		FileCsvUtil.convertListListString("");
+		// 增加处理函数接口，边杜读取文件，边处理行
+		FileCsvUtil.convertListListString("", null);
+	}
 }
