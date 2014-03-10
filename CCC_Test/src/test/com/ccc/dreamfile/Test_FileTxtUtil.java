7ca7c82147371412b77613d3f7a1f032b15d2d76
shaojianhong <shaojianhong@shao>
2014-03-10 09:11:29 +0800

    提交（增加测试用例）

--- CCC_Test/src/test/com/ccc/dreamfile/Test_FileTxtUtil.java
+++ CCC_Test/src/test/com/ccc/dreamfile/Test_FileTxtUtil.java
@@ -1,11 +1,12 @@
 package test.com.ccc.dreamfile;
 
 /**
- *
  * @author RedSword
  * @date 2014年3月8日 下午11:02:44
  */
 
 public class Test_FileTxtUtil {
-
+	public static void main(String[] args) {
+		// FileTxtUtil.
+	}
 }
