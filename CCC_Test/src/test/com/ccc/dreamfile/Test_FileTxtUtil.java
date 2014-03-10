37c2854db7bf9a5df3d4cb68f720f9f6d528215c
shaojianhong <shaojianhong@shao>
2014-03-10 15:02:41 +0800

    提交

--- CCC_Test/src/test/com/ccc/dreamfile/Test_FileTxtUtil.java
+++ CCC_Test/src/test/com/ccc/dreamfile/Test_FileTxtUtil.java
@@ -1,5 +1,11 @@
 package test.com.ccc.dreamfile;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ccc.dreamfile.txt.FileTxtUtil;
+
 /**
  * @author RedSword
  * @date 2014年3月8日 下午11:02:44
@@ -7,6 +13,27 @@ package test.com.ccc.dreamfile;
 
 public class Test_FileTxtUtil {
 	public static void main(String[] args) {
-		// FileTxtUtil.
+		List<String> listS = new ArrayList<String>() {
+			{
+				add("1,2,3");
+				add("2,3,1");
+			}
+		};
+
+		int count = FileTxtUtil.writeTxtFile(listS, new File("D:\\11.txt"), false);
+		System.out.println("写入txt文档:" + count);
+		System.out.println("将txt格式文本转换为List<String >格式 ");
+		List<String> li_s = FileTxtUtil.convertListString(new File("D:\\11.txt"));
+		for (String string : li_s) {
+			System.out.println(string);
+		}
+		System.out.println("将txt格式文本转换为List<String[]>格式（每行以后面的字符分隔）");
+		List<String[]> li = FileTxtUtil.convertListListString(new File("D:\\11.txt"), ",");
+		for (String[] string : li) {
+			for (String string2 : string) {
+				System.out.println(string2);
+			}
+			System.out.println();
+		}
 	}
 }
